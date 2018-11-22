63e9d9626c01f7c8cf9b6f9761c02c33c4ddfc67
Peter Kazazes <peter@peterk.co>
2018-11-21 18:20:06 -0800

    don't copy trunkplayer to data

--- host-images/trunk-player/start.sh
+++ host-images/trunk-player/start.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 
 source /trunk-player/env/bin/activate
-rm -rf /data/trunk-player
-cp -a /trunk-player /data
 cd /trunk-player
 
 ./manage.py migrate
 
 mkdir -p /data/web/static
-cp -a /trunk-player/static /data/web/static
+rsync -a --delete /trunk-player/static /data/web/static
 
 daphne trunk_player.asgi:channel_layer --port 7055 --bind 0.0.0.0 &
 

1882232c2682b1687c508a6a4492fdbd5e7be475
Peter Kazazes <peter@peterk.co>
2018-12-01 09:54:36 -0800

    soapyremote

--- host-images/soapy-remote/start.sh
+++ host-images/soapy-remote/start.sh
@@ -5,5 +5,6 @@ source /pybombs/setup_env.sh
 bladeRF-cli -p
 bladeRF-cli -l /usr/share/Nuand/bladeRF/hostedxA4.rbf
 
+SoapySDRUtil --find
 SoapySDRUtil --make="driver=bladeRF"
 SoapySDRServer --bind

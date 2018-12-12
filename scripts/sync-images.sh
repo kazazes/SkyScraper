fcfb626c36fbeb348c8cb5539eacda2c09ad6933
Peter Kazazes <peter@peterk.co>
2018-12-12 06:36:35 -0800

    qemu builds

--- scripts/sync-images.sh
+++ scripts/sync-images.sh
@@ -3,19 +3,40 @@
 set -e
 set -x
 
+# Pull GC Built images
 docker pull gcr.io/skyscraper-sdr/skyscraper-edge-amd64:latest
-docker tag gcr.io/skyscraper-sdr/skyscraper-edge-amd64:latest pckzs/bladerf-amd64
-docker push pckzs/bladerf-amd64
-
 docker pull gcr.io/skyscraper-sdr/skyscraper-edge-arm:latest
-docker tag gcr.io/skyscraper-sdr/skyscraper-edge-arm:latest pckzs/bladerf-arm
-docker push pckzs/bladerf-arm
+docker pull gcr.io/skyscraper-sdr/pybombs-amd64:latest
+docker pull gcr.io/skyscraper-sdr/pybombs-arm:latest
 
+# Tag for Docker Hub
+docker tag gcr.io/skyscraper-sdr/skyscraper-edge-arm:latest pckzs/bladerf-arm
+docker tag gcr.io/skyscraper-sdr/skyscraper-edge-amd64:latest pckzs/bladerf-amd64
+docker tag gcr.io/skyscraper-sdr/pybombs-amd64:latest pckzs/pybombs-amd64
+docker tag gcr.io/skyscraper-sdr/pybombs-arm:latest pckzs/pybombs-arm
+
+# Push before manifest
+docker push pckzs/bladerf-amd64
+docker push pckzs/bladerf-arm
+docker push pckzs/pybombs-amd64
+docker push pckzs/pybombs-arm
+
+# Manifest
 docker manifest create -a pckzs/bladerf \
   pckzs/bladerf-arm:latest \
   pckzs/bladerf-amd64:latest
 
+docker manifest create -a pckzs/pybombs \
+  pckzs/pybombs-arm:latest \
+  pckzs/pybombs-amd64:latest
+
+# Architectures
 docker manifest annotate pckzs/bladerf-arm:latest pckzs/bladerf --arch arm64
 docker manifest annotate pckzs/bladerf-amd64:latest pckzs/bladerf --arch amd64
 
-docker manifest push -p pckzs/bladerf:latest
+docker manifest annotate pckzs/pybombs-arm:latest pckzs/pybombs --arch arm64
+docker manifest annotate pckzs/pybombs-amd64:latest pckzs/pybombs --arch amd64
+
+# Push multi-arch image
+docker manifest push -p pckzs/bladerf
+docker manifest push -p pckzs/pybombs

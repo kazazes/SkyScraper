cd0c098f2417699af113108467459ccd3dc680f0
Peter Kazazes <peter@peterk.co>
2018-12-11 22:02:15 -0800

    cloudbuild confs

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,13 +5,12 @@ set -e
 cd base-images/bladerf
 
 docker build --pull -t pckzs/bladerf-arm -f armhf.dockerfile .
-docker push pckzs/bladerf-arm
 
 docker build --pull -t pckzs/bladerf-amd -f amd64.dockerfile .
-docker push pckzs/bladerf-amd
 
 docker manifest create pckzs/bladerf \
   pckzs/bladerf-arm:latest \
   pckzs/bladerf-amd64:latest
 
-docker manifest push -p docker.io/pckzs/bladerf:latest
+docker manifest annotate pckzs/bladerf-arm:latest pckzs/bladerf --arch arm
+docker manifest annotate pckzs/bladerf-amd64:latest pckzs/bladerf --arch amd64
---
+++ scripts/sync-images.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -e
+
+docker pull gcr.io/skyscraper-sdr/skyscraper-edge-amd64:latest
+docker tag gcr.io/skyscraper-sdr/skyscraper-edge-amd64:latest pckzs/bladerf-amd64
+
+docker pull gcr.io/skyscraper-sdr/skyscraper-edge-arm:latest
+docker tag gcr.io/skyscraper-sdr/skyscraper-edge-arm:latest pckzs/bladerf-arm
+
+docker manifest create pckzs/bladerf \
+  pckzs/bladerf-arm:latest \
+  pckzs/bladerf-amd64:latest
+
+docker manifest annotate pckzs/bladerf-arm:latest pckzs/bladerf --arch arm
+docker manifest annotate pckzs/bladerf-amd64:latest pckzs/bladerf --arch amd64
+
+docker manifest push -p docker.io/pckzs/bladerf:latest

f5d329c94f5adb55bbd159cab65a5027c481abdd
Peter Kazazes <peter@peterk.co>
2018-12-14 02:24:14 -0800

    expose trunk-player ports

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-cd base-images/bladerf
+cd base-images/pybombs
 
-docker build --pull -t pckzs/bladerf-arm --cache-from=gcr.io/skyscraper-sdr/skyscraper-edge-arm -f armhf.dockerfile .
-docker build --pull -t pckzs/bladerf-amd --cache-from=gcr.io/skyscraper-sdr/skyscraper-edge-amd64 -f amd64.dockerfile .
+docker build --pull -t pckzs/pybombs-arm --cache-from=gcr.io/skyscraper-sdr/pybombs-arm -f armhf.dockerfile .
+docker build --pull -t pckzs/pybombs-amd64 --cache-from=gcr.io/skyscraper-sdr/pybombs-amd64 -f amd64.dockerfile .
 
-docker manifest create pckzs/bladerf \
-  pckzs/bladerf-arm:latest \
-  pckzs/bladerf-amd64:latest
+docker manifest create -a pckzs/pybombs \
+  pckzs/pybombs-arm:latest \
+  pckzs/pybombs-amd64:latest
 
-docker manifest annotate pckzs/bladerf-arm:latest pckzs/bladerf --arch arm
-docker manifest annotate pckzs/bladerf-amd64:latest pckzs/bladerf --arch amd64
+docker manifest annotate  pckzs/pybombs pckzs/pybombs-arm:latest --arch arm64 --os linux
+docker manifest annotate  pckzs/pybombs pckzs/pybombs-amd64:latest --arch amd64

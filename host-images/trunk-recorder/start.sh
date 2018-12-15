a9fceaa1b049abb76cbbb33522ddd51de89de689
Peter Kazazes <peter@peterk.co>
2018-12-15 02:40:08 -0800

    specify armv7l for volk

--- host-images/trunk-recorder/start.sh
+++ host-images/trunk-recorder/start.sh
@@ -1,11 +1,17 @@
-#! /bin/sh
+#! /bin/bash
 
-. /usr/local/setup_env.sh
-. /skyscraper/src/trunk-player/env/bin/activate
+source /usr/local/setup_env.sh
+source /skyscraper/src/trunk-player/env/bin/activate
 
 wget https://www.nuand.com/fpga/hostedxA4-latest.rbf
 
 cp /data/conf/trunk-player/settings_local.py /skyscraper/src/trunk-player/trunk_player/
+
+arch=$(uname -i)
+if  [[ $arch == armv7l ]]; then
+  mkdir -p /root/.volk
+  cp /data/conf/volk_config /root/.volk/volk_config
+fi
 
 bladeRF-cli -p
 bladeRF-cli -l ./hostedxA4-latest.rbf

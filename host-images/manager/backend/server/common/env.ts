281ac553524524046cd0435f022deed362cbbb3b
Peter Kazazes <peter@peterk.co>
2018-12-16 02:47:03 -0800

    Use local or default dotenv

--- host-images/manager/backend/server/common/env.ts
+++ host-images/manager/backend/server/common/env.ts
@@ -1,2 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv";
+import { existsSync } from "fs";
+
+if (existsSync("./.env")) {
+  dotenv.config();
+} else {
+  dotenv.config({ path: "/data/conf/manager/production.env" });
+}

8c4ef742f2b78f8238d8cde19baaca405b6cc934
Peter Kazazes <peter@peterk.co>
2019-03-02 06:31:59 -0800

    static dist

--- host-images/manager/docker-entrypoint.sh
+++ host-images/manager/docker-entrypoint.sh
@@ -2,5 +2,8 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
+mkdir -p /data/www/
+cp -a /app/packages/frontend/dist/ /data/www/manager/
+
 cd /app/packages/server/
 yarn run start

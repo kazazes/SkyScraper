9de4ff7d5f71b1ed82fc4929879ed37095d9aa82
Peter Kazazes <peter@peterk.co>
2019-03-02 15:25:36 -0800

    touch log

--- host-images/manager/docker-entrypoint.sh
+++ host-images/manager/docker-entrypoint.sh
@@ -5,6 +5,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 mkdir -p /data/www/
 rm -rf /data/www/*
 cp -r /app/packages/frontend/dist/* /data/www/
+touch /data/log/mosquitto.log
 
 cd /app/packages/server/
 yarn run start

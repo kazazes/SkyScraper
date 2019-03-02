3a27dadfe35bf91b4b8e813080156210e13248dd
Peter Kazazes <peter@peterk.co>
2019-03-02 07:18:45 -0800

    manager: cachebust

--- host-images/manager/docker-entrypoint.sh
+++ host-images/manager/docker-entrypoint.sh
@@ -4,7 +4,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 mkdir -p /data/www/
 rm -rf /data/www/*
-cp -a /app/packages/frontend/dist/ /data/www/
+cp -r /app/packages/frontend/dist/* /data/www/
 
 cd /app/packages/server/
 yarn run start

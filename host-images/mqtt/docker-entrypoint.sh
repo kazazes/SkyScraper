c4d9f50014ee53badd417402cb0a1ec195842ffe
Peter Kazazes <peter@peterk.co>
2019-03-02 16:17:33 -0800

    mqtt: data permissions

--- host-images/mqtt/docker-entrypoint.sh
+++ host-images/mqtt/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 mkdir -p /data/mosquitto
-mkdir -p /data/log
+mkdir -p /data/log/
+
+touch /data/log/mosquitto.log
+chown 1883:1883 /data/mosquitto/ -R
+chmod o+w /data/log/mosquitto.log
+
+tail -f /data/log/mosquitto.log &
 
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf

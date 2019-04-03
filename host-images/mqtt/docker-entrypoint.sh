f02cb301d876df67c8c008587138a71002695acd
Peter Kazazes <peter@peterk.co>
2019-04-03 08:31:18 -0700

    Triage MQTT file permissions errors

--- host-images/mqtt/docker-entrypoint.sh
+++ host-images/mqtt/docker-entrypoint.sh
@@ -4,8 +4,8 @@ mkdir -p /data/mosquitto
 mkdir -p /data/log/
 
 touch /data/log/mosquitto.log
-chown 1883:1883 /data/mosquitto/ -R
-chmod o+w /data/log/mosquitto.log
+chown mosquitto:mosquitto -R /data/mosquitto/
+chmod 0666 /data/mosquitto
 
 tail -f /data/log/mosquitto.log &
 

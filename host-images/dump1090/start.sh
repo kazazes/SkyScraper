0df562db8e81e45f407575970e2651b06c45d62d
Peter Kazazes <peter@peterk.co>
2019-04-08 20:55:13 -0700

    dump1090: disable with ADSB_DISABLED

--- host-images/dump1090/start.sh
+++ host-images/dump1090/start.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [[ -v ADSB_DISABLED ]]; then
+    echo "ADSB disabled. Sleeping."
+    sleep infinity
+fi
+
 mkdir -p /data/adsb
 
 ./dump1090 --fix --enable-agc --net --write-json /data/adsb/adsb-$(date -Iminutes).json

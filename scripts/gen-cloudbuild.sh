c90cab67491e92bf91265fa382cd49ab00fa7937
Peter Kazazes <peter@peterk.co>
2019-04-16 04:03:08 -0700

    gcp: cb

---
+++ scripts/gen-cloudbuild.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+cd "$(dirname "$0")"/..
+PROJECT_DIR=$(pwd)
+
+PATH=/usr/local/opt/gnu-sed/libexec/gnubin/:$PATH
+
+set -e
+
+echo
+echo ===
+echo === If running on macOS, run
+echo === brew install gnu-sed
+echo ===
+
+
+function generate_cloudbuild() {
+    YAML=${PROJECT_DIR}/cloudbuild.yaml
+
+    echo "steps:" > $YAML
+    for D in ./host-images/*; do
+		if [[ -d "${D}" ]]; then
+		    IMAGE_URI=gcr.io/methodical-tea-237508/skyscraperai/$(basename ${D}):latest
+		    IMAGES="'$IMAGE_URI', $IMAGES"
+            echo -e "- name: 'gcr.io/cloud-builders/docker'" >> $YAML
+            echo -e "  args: ['build', '--cache-from', '${IMAGE_URI}' '-t', '${IMAGE_URI}', '${D}']" >> $YAML
+            echo -e "  timeout: 1200s" >> $YAML
+            echo -e "  waitFor: ['-']" >> $YAML
+            echo -e "- name: 'gcr.io/cloud-builders/docker'" >> $YAML
+            echo -e "  args: ['push', '${IMAGE_URI}']" >> $YAML
+		fi
+	done
+
+	echo -e "images: [$IMAGES]" >> $YAML
+#
+#	for D in ./base-images/*; do
+#		if [[ -d "${D}" ]]; then
+#
+#		fi
+#	done
+}
+generate_cloudbuild

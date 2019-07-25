19ed640391807fdc28445d123947b45dd886a729
Peter Kazazes <peter@peterk.co>
2019-07-25 00:39:48 -0700

    disiable wss

--- edge-images/frontend/plugins/apollo-config.ts
+++ edge-images/frontend/plugins/apollo-config.ts
@@ -1,11 +1,8 @@
 export default {
   connectToDevTools: true,
-  persisting: true,
-    httpEndpoint:
-  process.env.GRAPHQL_HTTP_ENDPOINT ||
-    "https://edge.sibyl.vision/graphql"
-    ,
-  wsEndpoint:
-    process.env.GRAPHQL_WS_ENDPOINT ||
- "wss://edge.sibyl.vision/graphql"
-}
+  httpEndpoint:
+    process.env.GRAPHQL_HTTP_ENDPOINT || "https://edge.sibyl.vision/graphql",
+  //   wsEndpoint:
+  //     process.env.GRAPHQL_WS_ENDPOINT ||
+  //  "wss://edge.sibyl.vision/graphql"
+};

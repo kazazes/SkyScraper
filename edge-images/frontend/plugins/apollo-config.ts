9d4091cc9605049ca2c3cd18737f002a80256e62
Peter Kazazes <peter@peterk.co>
2019-07-24 22:47:13 -0700

    force apollo devtools

--- edge-images/frontend/plugins/apollo-config.ts
+++ edge-images/frontend/plugins/apollo-config.ts
@@ -1,10 +1,9 @@
 export default {
+  connectToDevTools: true,
   persisting: true,
     httpEndpoint:
   process.env.GRAPHQL_HTTP_ENDPOINT ||
-  process.env.NODE_ENV === "production"
-    ? "https://edge.sibyl.vision/graphql" :
-    "http://127.0.0.1:4000/graphql"
+    "https://edge.sibyl.vision/graphql"
     ,
     httpLinkOptions: {
     credentials: "same-origin",

d5cdeee0010375ccebf0d4ff736b851edd35f8d4
Peter Kazazes <peter@peterk.co>
2019-07-26 15:46:57 -0700

    build wifi-connect from local sources

--- edge-images/wifi-connect/scripts/start.sh
+++ edge-images/wifi-connect/scripts/start.sh
@@ -2,6 +2,12 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
+# Optional step - it takes couple of seconds (or longer) to establish a WiFi connection
+# sometimes. In this case, following checks will fail and wifi-connect
+# will be launched even if the device will be able to connect to a WiFi network.
+# If this is your case, you can wait for a while and then check for the connection.
+# sleep 15
+
 # Choose a condition for running WiFi Connect according to your use case:
 
 # 1. Is there a default gateway?
@@ -20,8 +26,5 @@ if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
 else
     printf 'Starting WiFi Connect\n'
-    ./wifi-connect -s SkyScraper
+    ./wifi-connect
 fi
-
-# Start your application here.
-sleep infinity

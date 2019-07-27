296f15129b1004eed3f5d69d58f69f3ef553f6e8
Peter Kazazes <peter@peterk.co>
2019-07-26 19:05:22 -0700

    open chromium to frontend

--- edge-images/window-manager/start_x86.sh
+++ edge-images/window-manager/start_x86.sh
@@ -6,7 +6,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # rotate screen if env variable is set [normal, inverted, left or right]
 if [[ ! -z "$ROTATE_DISPLAY" ]]; then
   echo "YES"
-  (sleep 3 && DISPLAY=:0 xrandr -o $ROTATE_DISPLAY) & 
+  (sleep 3 && DISPLAY=:0 xrandr -o $ROTATE_DISPLAY) &
 fi
 
 # start desktop manager
@@ -17,4 +17,7 @@ echo "STARTING X"
 # startx -- -nocursor
 
 # uncomment to open an application instead of the desktop
-startx xterm  
+startx chromium http://frontend:3000 --incognito --no-sandbox --kiosk --window-size=1920,1080 \
+  --start-fullscreen --noerrdialogs --disable-translate --no-first-run \
+  --fast --fast-start --disable-infobars --disable-features=TranslateUI \
+  --disk-cache-dir=/dev/null

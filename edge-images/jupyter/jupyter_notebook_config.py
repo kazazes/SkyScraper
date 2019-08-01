3b121176686467ec7dfcfe13e9dbe52fc74bacaa
Peter Kazazes <peter@skyscraper.ai>
2019-07-31 23:42:06 -0700

    CSP allow local embed

--- edge-images/jupyter/jupyter_notebook_config.py
+++ edge-images/jupyter/jupyter_notebook_config.py
@@ -14,7 +14,7 @@ c.NotebookApp.open_browser = False
 c.NotebookApp.tornado_settings = {
     'headers': {
         'Content-Security-Policy':
-        "frame-ancestors 'self' https://*.sibyl.vision"
+        "frame-ancestors 'self' https://*.sibyl.vision http://127.0.0.1:3000 http://localhost:3000"
     }
 }
 

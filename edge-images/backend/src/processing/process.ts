d5845fdc3efbd2a28afd91bf12527e668690e562
Peter Kazazes <peter@peterk.co>
2019-08-09 06:49:35 -0700

    fix frontend transx sub

--- edge-images/backend/src/processing/process.ts
+++ edge-images/backend/src/processing/process.ts
@@ -2,28 +2,12 @@ import { TrunkedCall } from "../graphql/generated/prisma-client";
 
 import log from "../log";
 import { publish } from "../mqtt/broker";
-import CloudStorage from "./cloudStorage";
-import CloudPublish from "./transcription";
-
-const UUID = process.env.BALENA_DEVICE_UUID || "skyscraper-test";
-
-const storage = new CloudStorage(UUID);
 
 export const processTrunkedVoice = async (call: TrunkedCall) => {
-  if (process.env.ENABLE_UPLOAD === "1") {
-    try {
-      await storage.initialize();
-      const [mp3File] = await storage.uploadAudio(call.id, call.audioPath);
-      const [wavFile] = await storage.uploadAudio(call.id, call.wavPath);
-
-      log.info(`Uploaded ${mp3File.name} to bucket ${mp3File.bucket}`);
-      log.info(`Uploaded ${wavFile.name} to bucket ${wavFile.bucket}`);
-    } catch (e) {
-      log.error("Error uploading trunked voice to GCS", e);
-    }
-  }
-
-  if (process.env.ENABLE_TRANSCRIPTION && process.env.ENABLE_TRANSCRIPTION !== "0") {
+  if (
+    process.env.ENABLE_TRANSCRIPTION &&
+    process.env.ENABLE_TRANSCRIPTION !== "0"
+  ) {
     try {
       const topic = `transcription/${call.id}/request`;
       await publish(topic, JSON.stringify(call), 2);

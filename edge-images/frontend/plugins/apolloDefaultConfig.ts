558a38ab0c7c73c39f66b42ce0ba4b84b2f3e861
Peter Kazazes <peter@peterk.co>
2019-08-16 07:23:52 -0700

    missing path

--- edge-images/frontend/plugins/apolloDefaultConfig.ts
+++ edge-images/frontend/plugins/apolloDefaultConfig.ts
@@ -7,13 +7,13 @@ export default () => {
     persisting: true,
     httpEndpoint: isDev
       ? "http://127.0.0.1:4000/graphql"
-      : `https://edge.sibyl.vision`,
+      : `https://edge.sibyl.vision/graphql`,
     httpLinkOptions: {
       credentials: "same-origin",
     },
     wsEndpoint: isDev
       ? "ws://127.0.0.1:4000/graphql"
-      : `wss://edge.sibyl.vision`,
+      : `wss://edge.sibyl.vision/graphql`,
     websocketsOnly: false,
   };
 };

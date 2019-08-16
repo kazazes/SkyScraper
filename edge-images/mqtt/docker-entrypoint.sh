d893aa78a8c90abbab2ed6824baff31290c373bb
Peter Kazazes <peter@peterk.co>
2019-08-16 05:05:51 -0700

    add udid to mqtt upstream calls

--- edge-images/mqtt/docker-entrypoint.sh
+++ edge-images/mqtt/docker-entrypoint.sh
@@ -7,6 +7,8 @@ touch /data/log/mosquitto.log
 chown mosquitto:mosquitto -R /data/mosquitto/
 chmod 0666 /data/mosquitto
 
+envsubst < /mosquitto/config/mosquitto.conf > /mosquitto/config/mosquitto.conf
+
 tail -f /data/log/mosquitto.log &
 
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf

e017615cdd15a2d9644281d40721b36d877a01eb
Peter Kazazes <peter@peterk.co>
2019-08-16 01:46:30 -0700

    set DD_HOSTNAME to EDGE_HOSTNAME

--- edge-images/datadog/files/start.sh
+++ edge-images/datadog/files/start.sh
@@ -5,6 +5,11 @@ then
   exit 1
 fi
 
+if [ -z ${DD_HOSTNAME} ]
+then
+  export DD_HOSTNAME=$EDGE_HOSTNAME
+fi
+
 ln -sf /var/run/balena.sock /var/run/docker.sock
 
 echo "api_key: $DATADOG_API_KEY" | cat - files/datadog.yaml > temp && mv temp files/datadog.yaml
